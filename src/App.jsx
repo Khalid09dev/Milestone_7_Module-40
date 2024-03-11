e08cc34653a7f9418b3604e39a234bc8ccf337ba
khalid09dev <mdkhalidhossen@hotmail.com>
2024-03-11 22:34:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+  return (
+    <>
+      <h1>React World Tour</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
+
+
+//1. component
+//2. jsx
+//3. props
+//4. event handler
+//5. state
+//6. useEffect
